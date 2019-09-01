93aaffbc7847409e5d1938dbc4472c37fb07f8f0
Yury Lavrukhin <yurka7321@gmail.com>
2019-09-01 21:50:36 +0600

    Add gulp with postcss, .gitignore, husky, lint-staged, .eveventy.js, .editorconfig, .browserslistrc, prettier, netlify.toml

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = {
+  templateFormats: ['njk', 'md', 'png'],
+  dir: {
+    input: 'src/site',
+    output: 'dist',
+  },
+}
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+
+gulp.task('css', () => {
+  const postcss = require('gulp-postcss')
+  const sourcemaps = require('gulp-sourcemaps')
+
+  return gulp
+    .src('./src/styles/index.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss())
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('./src/**/*.css', gulp.series('css'))
+})
+
+gulp.task('default', (done) => {
+  gulp.parallel('css')(done)
+})
