f166de253aa47b572028dcb4b2980523c1b66eb8
Yury Lavrukhin <yurka7321@gmail.com>
2019-09-07 00:45:42 +0600

    Install postcss-nested plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('autoprefixer'), require('postcss-easy-import')],
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-easy-import'),
+    require('postcss-nested'),
+  ],
 }
