007070c076eb662317f797cd58724db3d8a88dd2
Yury Lavrukhin <yurka7321@gmail.com>
2019-09-07 19:05:09 +0600

    update colors link logic

--- src/site/assets/scripts/main.js
+++ src/site/assets/scripts/main.js
@@ -1,14 +1,16 @@
 const colors = ['ffb74d', '8bcfee', '51f1be', 'ec6a6a']
-const isSetInitialLoadingSSKey = 'isSetInitialLoading'
-
-const isSetInitialLoading = sessionStorage.getItem(isSetInitialLoadingSSKey)
+const isSetInitialLoading = sessionStorage.getItem('isSetInitialLoading')
 
 if (!isSetInitialLoading) {
-  const ranNum = Math.floor(Math.random() * (colors.length - 1))
-  document.documentElement.style.setProperty(
-    '--color-primary-links',
-    `#${colors[ranNum]}`,
+  sessionStorage.setItem(
+    'randomColorIndex',
+    Math.floor(Math.random() * colors.length),
   )
 }
 
-sessionStorage.setItem(isSetInitialLoadingSSKey, true)
+document.documentElement.style.setProperty(
+  '--color-primary-links',
+  `#${colors[sessionStorage.getItem('randomColorIndex')]}`,
+)
+
+sessionStorage.setItem('isSetInitialLoading', true)
