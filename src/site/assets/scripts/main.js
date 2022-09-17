d6dd8da1032a81b4688aebfff3f23d01931cad88
Yury Lavrukhin <me@yurylavrukhin.com>
2022-09-17 16:02:48 +0700

    feat: bg noisy gradient

--- src/site/assets/scripts/main.js
+++ src/site/assets/scripts/main.js
@@ -1,4 +1,5 @@
-const colors = ['ffb74d', '8bcfee', '51f1be', 'ec6a6a']
+// const colors = ['ffb74d', '8bcfee', '51f1be', 'ec6a6a']
+const colors = ['ffb74d']
 const isSetInitialLoading = sessionStorage.getItem('isSetInitialLoading')
 
 if (!isSetInitialLoading) {
